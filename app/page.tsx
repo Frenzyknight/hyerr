cdda7d1379be2bc925e90a08572310e3ff9b6b19
gauravganju-donoriq <gauravg@ignitecinc.com>
2026-05-01 12:37:20 +0530

    added mobile gif

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="relative min-h-dvh w-full overflow-hidden bg-white">
-      <Image
-        src="/Hyerr.gif"
-        alt="Hyerr coming soon"
-        fill
-        priority
-        unoptimized
-        sizes="100vw"
-        className="object-contain sm:object-cover"
-      />
+      <picture>
+        <source media="(max-width: 639px)" srcSet="/Hyerr_Mobile.gif" />
+        <img
+          src="/Hyerr.gif"
+          alt="Hyerr coming soon"
+          className="h-dvh w-full object-cover"
+          fetchPriority="high"
+        />
+      </picture>
     </main>
   );
 }
